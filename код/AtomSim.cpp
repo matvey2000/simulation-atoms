2da8402b01a88559b1b4420f37c2dae1aafbe26f
matvey2000 <35378456+matvey2000@users.noreply.github.com>
2021-10-13 20:28:42 +0300

    Add files via upload

--- код/AtomSim.cpp
+++ код/AtomSim.cpp
@@ -17,6 +17,7 @@ public:
     float vy = 0;
 
     int type;//тип
+    std::list<int> svz;//связи частицы (номера связанных частиц)
 };
 std::list<atom> atms;//атом лист
 
@@ -53,22 +54,52 @@ void render(float rd, int** colmat, int types)
     }
     window.display();
 };
-void itt(float* ms, int types, float* qs, float k, float dt, float a, float b)
+void itt(float* ms, int types, float* qs, float k, float dt, float a, float b, float rd, float rs, float endurance_s)
 {
     float r;
 
     atom atm;
+    int n1 = -1;
     for (auto atmz = std::begin(atms); atmz != std::end(atms); atmz++)
     {
+        n1++;
         atm = *atmz;
+        int n0 = -1;
         for (auto atm2 : atms)
         {
+            bool svz = false;//наличие связи
+
+            n0++;
+            for (auto n : atm.svz)
+            {
+                if (n0 == n)
+                {
+                    svz = true;
+                    break;
+                };
+            };
+
+            r = (k * qs[atm.type] * qs[atm2.type]) / (ms[atm.type] * (pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)));
             //отталкивание
             if (atm2.x != atm.x || atm2.y != atm.y)
             {
-                r = (k * qs[atm.type] * qs[atm2.type]) / (ms[atm.type] * (pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)));
-                atm.vx += (atm2.x - atm.x) / sqrt(pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)) * r;
-                atm.vy += (atm2.y - atm.y) / sqrt(pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)) * r;
+                atm.vx -= dt * (atm2.x - atm.x) / sqrt(pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)) * r;
+                atm.vy -= dt * (atm2.y - atm.y) / sqrt(pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)) * r;
+            }
+
+            //образование связей
+            if (sqrt(pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)) <= (rd * 2 + rs) && (!svz) && (atm2.x != atm.x || atm2.y != atm.y))
+            {
+                std::cout << n0 << " " << n1 << " " << "\n";
+                atm.svz.push_back(n0);
+                atm2.svz.push_back(n1);
+            } 
+
+            //притяжение связей
+            if (svz)
+            {
+                atm.vx += dt * (atm2.x - atm.x) / sqrt(pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)) * endurance_s;
+                atm.vy += dt * (atm2.y - atm.y) / sqrt(pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)) * endurance_s;
             }
         };
 
@@ -88,7 +119,7 @@ void itt(float* ms, int types, float* qs, float k, float dt, float a, float b)
 int main()
 {
     //параметры
-    int types = 2;//кол-во типов атомов
+    int types = 3;//кол-во типов атомов
     int a = w;//ширина пространства игры(x)
     int b = h;//длинна(y)
     int n = 50;//кол-во атомов при старте
@@ -96,6 +127,7 @@ int main()
     float dt = 1.;//время шага симуляции
     float k = 100;//коэффициент отталкивания
     float rs = 5.;//расстояние связи - радиус * 2
+    float endurance_s = 0.;//прочность связи
 
     int** colmat = new int*[types];//цвета типов
     float* ms = new float[types];//масса типов
@@ -110,15 +142,21 @@ int main()
     colmat[0][1] = 191;
     colmat[0][2] = 255;
 
-    colmat[0][0] = 102;
-    colmat[0][1] = 255;
-    colmat[0][2] = 0;
+    colmat[1][0] = 102;
+    colmat[1][1] = 255;
+    colmat[1][2] = 0;
+
+    colmat[2][0] = 220;
+    colmat[2][1] = 20;
+    colmat[2][2] = 60;
 
     ms[0] = 1;
     ms[1] = 1.5;
+    ms[2] = 1.2;
     
     qs[0] = 1.;
     qs[1] = 1.2;
+    qs[2] = 1.2;
     /*
     esc - выход
     */
@@ -143,7 +181,7 @@ int main()
             }
         }
         
-        itt(ms, types, qs, k, dt, a, b);
+        itt(ms, types, qs, k, dt, a, b, rd, rs, endurance_s);
         render(rd, colmat, types);
     }
 }
