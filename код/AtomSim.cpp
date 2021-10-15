2658a97f38edeb62a20b1dccd19f15f1c83f78da
matvey2000 <35378456+matvey2000@users.noreply.github.com>
2021-10-15 18:15:27 +0300

    Add files via upload

--- код/AtomSim.cpp
+++ код/AtomSim.cpp
@@ -3,6 +3,8 @@
 #include <list>
 #include <random>
 
+float pi = 3.141592653589793238462643;
+
 int w = sf::VideoMode::getDesktopMode().width;
 int h = sf::VideoMode::getDesktopMode().height;
 
@@ -46,15 +48,49 @@ void render(float rd, int** colmat, int types)
     window.clear();
     for (auto atm : atms)
     {
+        //частица
         atm_r.setFillColor(sf::Color::Color(colmat[atm.type][0], colmat[atm.type][1], colmat[atm.type][2]));
-
-        atm_r.setPosition(sf::Vector2f(atm.x + w / 2, atm.y + h / 2));
+        atm_r.setPosition(sf::Vector2f(atm.x + w / 2 - rd / 2, atm.y + h / 2 - rd / 2));
 
         window.draw(atm_r);
+
+        //связи
+        for (auto svz : atm.svz)
+        {
+            atom atm2;
+            int n = -1;
+            for (auto atm3 : atms)
+            {
+                n++;
+                if (n == svz)
+                {
+                    atm2 = atm3;
+                    break;
+                };
+            }
+
+            sf::ConvexShape svz1;
+            svz1.setPointCount(4);
+            svz1.setFillColor(sf::Color::Color(colmat[atm.type][0], colmat[atm.type][1], colmat[atm.type][2]));
+
+            float xm = (atm2.x - atm.x) / sqrt(pow(atm.x - atm2.x,2) + pow(atm2.y - atm.y,2));
+            float ym = (atm2.y - atm.y) / sqrt(pow(atm.x - atm2.x, 2) + pow(atm2.y - atm.y, 2));
+
+            sf::VertexArray lines(sf::Lines, 2);
+            lines;
+
+            lines[0].position = sf::Vector2f(atm.x + w / 2, atm.y + h / 2);
+            lines[1].position = sf::Vector2f(atm2.x + w / 2, atm2.y + h / 2);
+
+            lines[0].color = sf::Color::Color(colmat[atm.type][0], colmat[atm.type][1], colmat[atm.type][2]);
+            lines[1].color = sf::Color::Color(colmat[atm2.type][0], colmat[atm2.type][1], colmat[atm2.type][2]);
+
+            window.draw(lines);
+        }
     }
     window.display();
 };
-void itt(float* ms, int types, float** qs, float k, float dt, float a, float b, float rd, float rs, float endurance_s)
+void itt(float* ms, int types, float** qs, float k, float dt, float a, float b, float rd, float rs, float endurance_s, float ed, float edst)
 {
     float r;
 
@@ -92,7 +128,6 @@ void itt(float* ms, int types, float** qs, float k, float dt, float a, float b, 
             {
                 std::cout << n0 << " " << n1 << " " << "\n";
                 atm.svz.push_back(n0);
-                atm2.svz.push_back(n1);
             } 
 
             //притяжение связей
@@ -101,20 +136,51 @@ void itt(float* ms, int types, float** qs, float k, float dt, float a, float b, 
                 atm.vx += dt * (atm2.x - atm.x) / sqrt(pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)) * endurance_s;
                 atm.vy += dt * (atm2.y - atm.y) / sqrt(pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)) * endurance_s;
             }
+            
+            //столкновения
+            if ((atm2.x != atm.x || atm2.y != atm.y) && sqrt(pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)) <= rd * 2)
+            {
+                atm.vx -= ed * dt * (atm2.x - atm.x) / sqrt(pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)) / (sqrt(pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)) - rd);
+                atm.vy -= ed * dt * (atm2.y - atm.y) / sqrt(pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)) / (sqrt(pow(atm.x - atm2.x, 2) + pow(atm.y - atm2.y, 2)) - rd);
+            }
         };
+
+        //запись
+        atmz = atms.erase(atmz);
+        atms.insert(atmz, atm);
+    };
+    n1 = -1;
+    for (auto atmz = std::begin(atms); atmz != std::end(atms); atmz++)
+    {
+        n1++;
+        atm = *atmz;
 
         //скорость
         atm.x += atm.vx * dt;
         atm.y += atm.vy * dt;
 
-        //закольцовка
-        atm.x = fmod((atm.x + a/2), a) - a/2;
-        atm.y = fmod((atm.y + b/2), b) - b/2;
-        
+        //ограничение
+        if (atm.x >= w / 2)
+        {
+            atm.vx += -dt * edst;
+        }
+        else if (atm.x <= -w / 2)
+        {
+            atm.vx = dt * edst;
+        }
+        if (atm.y >= h / 2)
+        {
+            atm.y = -dt * edst;
+        }
+        else if (atm.y <= -h / 2)
+        {
+            atm.vy = dt * edst;
+        }
+
         //запись
         atmz = atms.erase(atmz);
         atms.insert(atmz, atm);
-    };
+    }
 }
 int main()
 {
@@ -128,6 +194,8 @@ int main()
     float k = 100;//коэффициент отталкивания
     float rs = 5.;//расстояние связи - радиус * 2
     float endurance_s = 0.1;//прочность связи
+    float ed = 1.;//сопротивление атома проникновению внутрь себя
+    float edst = 1.;;//сопротивление стенки проникновению внутрь себя
 
     int** colmat = new int*[types];//цвета типов
     float* ms = new float[types];//масса типов
@@ -162,12 +230,12 @@ int main()
     qs[0][1] = 1;//отталкивание частиц № 1 и 2
     qs[0][2] = 0;//отталкивание частиц № 1 и 3
 
-    qs[1][0] = -1;
+    qs[1][0] = 1;
     qs[1][1] = 0;
-    qs[1][2] = 1;
+    qs[1][2] = 2;
 
     qs[2][0] = 0;
-    qs[2][1] = 0;
+    qs[2][1] = 2;
     qs[2][2] = 1;
     /*
     esc - выход
@@ -193,7 +261,7 @@ int main()
             }
         }
         
-        itt(ms, types, qs, k, dt, a, b, rd, rs, endurance_s);
+        itt(ms, types, qs, k, dt, a, b, rd, rs, endurance_s, ed, edst);
         render(rd, colmat, types);
     }
 }
